c2c61e40c4fae6d279c13b82fab6b7bead37b54f
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-08-10 15:39:40 +0530

    global variables in node

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const sam = "sam";
 const sayHi = require("./utils");
 sayHi(peter);
 sayHi(sam);
+
+//global variables in node: dirname, filename, require, module
+console.log(__dirname);
+console.log(__filename)
