75dce25d890dab5e8c2fe3930cb43fea3b00d463
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-08-10 16:48:23 +0530

    path module in node.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// built in modules: os, path, fs, http
+// Built in modules: os, path, fs, http
 
-// os module
+// 1] os module
 
 const os = require('os');
-console.log(os)
+// console.log(os)
 const userData = {
     userInfo: os.userInfo(),
     hostname: os.hostname(),
@@ -16,3 +16,18 @@ const userData = {
     machineInfo: os.machine()
 }
 console.log(userData);
+
+// 2] path module
+
+const path = require('path');
+console.log(path)
+//path seperator
+console.log(path.sep);
+//relative path
+const userPath = path.join("/main","subFolder","utils.js");
+console.log(userPath);
+// absolute path
+console.log(path.resolve(__dirname,"main","subFolder","utils.js"));
+// base file 
+console.log(path.basename(userPath));
+
