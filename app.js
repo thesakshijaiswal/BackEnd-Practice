f7cbed3545b59af4875555f3578e2e58783ec45f
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-08-10 16:34:46 +0530

    os module in node.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-require('./utils');
+// built in modules: os, path, fs, http
+
+// os module
+
+const os = require('os');
+console.log(os)
+const userData = {
+    userInfo: os.userInfo(),
+    hostname: os.hostname(),
+    osType: os.type(),
+    version: os.version(),
+    release: os.release(),
+    totalmem: os.totalmem(),
+    freemem: os.freemem(),
+    uptime: os.uptime(),
+    machineInfo: os.machine()
+}
+console.log(userData);
