6cdb5f57124937b7d59219f29368ea7d5e855b56
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-08-10 21:37:15 +0530

    fs module in node.js

--- app.js
+++ app.js
@@ -17,10 +17,12 @@ const userData = {
 }
 console.log(userData);
 
+
+
 // 2] path module
 
 const path = require('path');
-console.log(path)
+// console.log(path)
 //path seperator
 console.log(path.sep);
 //relative path
@@ -31,3 +33,44 @@ console.log(path.resolve(__dirname,"main","subFolder","utils.js"));
 // base file 
 console.log(path.basename(userPath));
 
+
+
+// 3] fs:(file system) module
+
+//read and write file synchronously!
+
+const fs = require('fs');
+// console.log(fs)
+console.log('start');
+const read = fs.readFileSync("./main/subFolder/msg.txt","utf8");
+fs.writeFileSync("./main/subFolder/resultSync.txt",`the result message: ${read}`,{flag: 'a'});
+console.log('task completed');
+console.log(read);
+console.log('loading next task');
+
+//read and write file Asynchronously!
+
+console.log('start');
+fs.readFile("./main/subFolder/msg.txt","utf8",(err,result) => {
+  if(err){
+    console.log(err);
+    return;
+  }
+  const firstResult = result;
+  fs.readFile("./main/subFolder/msg-2.txt","utf8",(err,result) => {
+  if(err){
+    console.log(err);
+    return;
+  }
+  const secondResult = result;
+  fs.writeFile("./main/subFolder/async-result.txt",`the combined msg: ${firstResult}, ${secondResult}`,(err,result) => {
+  if(err){
+    console.log(err);
+    return;
+  }
+  console.log(result);
+  console.log('task completed');
+})
+})
+})
+console.log('loading next task');
