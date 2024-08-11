6fe5199ade5bb28f84bbd9a1eada081c7c277f47
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-08-11 21:21:12 +0530

    create 404 error page

--- Lect-2-http-module/app.js
+++ Lect-2-http-module/app.js
@@ -1,8 +1,22 @@
+// 4] http module
+
 const http = require('http');
 // console.log(http);
 const server = http.createServer((req,res) => {
-  res.write("Welcome to our Home page!");
-  res.end();
+//  console.log(req)
+if(req.url === '/'){
+    res.end("Welcome to our Home page!");
+    return;
+}
+if(req.url === '/about'){
+    res.end("know more about us");
+    return;
+}
+    res.end(`
+        <h1>Oops!</h1>
+        <p>we are unable to find a page you are looking for!</p>
+        <a href="/">back to Home</a>
+        `);
 })
 
 server.listen(5000);
